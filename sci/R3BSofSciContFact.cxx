95f91588cd29727cdad327ad9259e6b11d589e33
Audrey Chatillon <audrey.chatillon@gmail.com>
2019-11-27 22:43:15 +0100

    implement the class R3BSofSciContFact

---
+++ sci/R3BSofSciContFact.cxx
@@ -0,0 +1,64 @@
+#include "R3BSofSciContFact.h"
+
+#include "FairLogger.h"
+#include "FairParAsciiFileIo.h"
+#include "FairParRootFileIo.h"
+#include "FairRuntimeDb.h"
+
+#include "R3BSofSciPar.h"
+
+#include "TClass.h"
+
+static R3BSofSciContFact gR3BSofSciContFact;
+
+R3BSofSciContFact::R3BSofSciContFact()
+{
+    // Constructor (called when the library is loaded)
+    fName = "R3BSofSciContFact";
+    fTitle = "Factory for Sci parameter containers in libR3BSofSci";
+    setAllContainers();
+    LOG(INFO) << "Enter in the R3BSofSciContFact constructor";
+    FairRuntimeDb::instance()->addContFactory(this);
+}
+
+void R3BSofSciContFact::setAllContainers()
+{
+    /** Creates the Container objects with all accepted contexts and adds them to
+     *  the list of containers for the STS library.*/
+
+    // --- --------------------------------------------------------------------------- --- //
+    // --- for the hit finder in the scintillator at cave C when no scintillator at S2 --- //
+    // --- --------------------------------------------------------------------------- --- //
+    FairContainer* p1 = new FairContainer("SofSciSingleTcalPar", "Sof Sci Parameters", "SofSciParContext");
+    p1->addContext("SofSciParContext");
+    containers->Add(p1);
+
+
+
+
+}
+
+FairParSet* R3BSofSciContFact::createContainer(FairContainer* c)
+{
+    /** Trals the constructor of the corresponding parameter container.
+     * For an actual context, which is not an empty string and not the default context
+     * of this container, the name is concatinated with the context. */
+
+    const char* name = c->GetName();
+    LOG(INFO) << "R3BSofSciContFact: Create container name: " << name;
+    FairParSet* p = 0;
+
+    if (strcmp(name, "SofSciSingleTcalPar") == 0)
+    {
+        p = new R3BSofSciPar(c->getConcatName().Data(), c->GetTitle(), c->getContext());
+    }
+    return p;
+}
+
+void R3BSofSciContFact::activateParIo(FairParIo* io)
+{
+    // activates the input/output class for the parameters
+    // needed by the Sts
+}
+
+ClassImp(R3BSofSciContFact)
