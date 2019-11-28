60ef6b7c0171d677cac7de7f8c34976c85a0b274
Audrey Chatillon <audrey.chatillon@gmail.com>
2019-11-28 10:37:17 +0100

    add tcal parameters for SofSci and ToFW: not all channels calibrated

--- sci/SofSciLinkDef.h
+++ sci/SofSciLinkDef.h
@@ -4,7 +4,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class R3BSofSCI+;
+//#pragma link C++ class R3BSofSCI+;
 
 #pragma link C++ class R3BSofSciMapped2TcalPar+;
 #pragma link C++ class R3BSofSciMapped2Tcal+;
